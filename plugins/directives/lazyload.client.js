72b9bf7f25b8ab5fc10ee3d42d7db85a792ce6ef
João Olivio Neto <joaoolivioneto@gmail.com>
2021-06-17 23:50:55 -0300

    Components: Adds lazyload and animate on scroll directives

---
+++ plugins/directives/lazyload.client.js
@@ -0,0 +1,41 @@
+import { debounce } from 'lodash';
+import Vue from 'vue';
+
+const ROOT_MARGIN = '150px 150px 150px 150px';
+const DEFAULT_THRESHOLD = [0.006];
+
+const getMatchingImageToMedia = (backgroundList, viewportWidth = document.documentElement.clientWidth) => {
+  if (typeof backgroundList === 'string') return backgroundList;
+
+  const breakpoints = Object.keys(backgroundList).reverse();
+
+  const matching = breakpoints.filter(breakpoint => viewportWidth >= breakpoint).shift();
+
+  return backgroundList[matching];
+}
+
+Vue.directive('lazyload', {
+  inserted: (el, binding) => {
+    const updateCurrentImage = () => {
+      const url = getMatchingImageToMedia(binding.value);
+      el.src = url;
+    }
+
+    const imageIntersectionObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting || entry.intersectionRatio > 0) {
+            updateCurrentImage();
+            el.resizeListener = debounce(updateCurrentImage, 300);
+            window.addEventListener('resize', el.resizeListener);
+          }
+        })
+      },
+      {ROOT_MARGIN, DEFAULT_THRESHOLD}
+    )
+  },
+  unbind: el => {
+    if (el.resizeListener) window.removeEventListener('resize', el.resizeListener);
+  }
+})
+
